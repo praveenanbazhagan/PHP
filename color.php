b2f5707272b9b7978ca1861aced5e36ae4f86a06
praveenanbazhagan <40654043+praveenanbazhagan@users.noreply.github.com>
2018-07-30 18:52:49 +0530

    Add files via upload

---
+++ color.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html>
+	<body>
+	
+
+	<?php
+	$color = "red";
+	$COLOR = "blue";
+	$coLOR = "RED";
+	echo "My car is " . $color . "<br>";
+	echo "My house is " . $COLOR . "<br>";
+	echo "My boat is " . $coLOR . "<br>";
+	?> 
+	
+
+	</body>
+	</html>
+
---
+++ text.php
@@ -0,0 +1,3 @@
+<?PHP
+echo 'This is praveen<br />';
+?>
